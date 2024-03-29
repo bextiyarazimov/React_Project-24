a3faa3cc48335fbf6eb388b794f7369af5c1e791
Baxtiyar Azimov <baxtiyar.azimov@aahm.lan>
2024-03-29 16:45:41 +0400

    add

---
+++ react_24/proje24/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './componenets/Header';
+import Footer from './componenets/Footer';
+import Home from './componenets/Home';
+import About from './componenets/About';
+import Contact from './componenets/Contact';
+import Product from './componenets/Product';
+import {Route, Switch} from 'react-router-dom';
+
+
+
+
+function App() {
+  return (
+   <>
+   <Switch>
+<Route></Route>
+   </Switch>
+
+   </>
+  );
+}
+
+export default App;
